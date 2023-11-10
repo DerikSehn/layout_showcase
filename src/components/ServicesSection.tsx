1add7a9f1a994e2e148ac9718a10a0edb2a207bd
Derik <derikbosing@gmail.com>
2023-11-10 01:59:40 -0300

    V1

---
+++ src/components/ServicesSection.tsx
@@ -0,0 +1,15 @@
+// src/components/ServicesSection.tsx
+import React from 'react';
+
+const ServicesSection: React.FC = () => {
+  return (
+    <section className="bg-gray-200 p-8">
+      <div className="container mx-auto">
+        <h2 className="text-2xl font-semibold mb-4 px-4">Nossos Serviços</h2>
+        <p className='px-4'>Descubra nossos serviços de catering para eventos especiais.</p>
+      </div>
+    </section>
+  );
+};
+
+export default ServicesSection;
